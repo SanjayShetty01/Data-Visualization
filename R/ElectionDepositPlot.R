0ea82c3dbebe88ae7712efa0a30be143b69b709d
SanjayShetty01 <shettysanjay01@gmail.com>
2022-03-24 10:35:48 +0530

    completed ElectionDepositPlot.R

--- R/ElectionDepositPlot.R
+++ R/ElectionDepositPlot.R
@@ -1,31 +1,37 @@
+### Load the Library
+
 library(dplyr)
 library(rvest)
 library(ggplot2)
 library(ggpubr)
 
+## Load the data 
 
 data = read.csv('TCPD_GE_all_2022-3-23.csv')
 
 ## Congress 
 
+### Extracting Congress data
 dataINC = data |>
   subset(Party_ID == '3482')
 
 
 head(dataINC)
 
+### Freq Distribution
 table(dataINC$Deposit_Lost)
 
-
+### Selecting the necessary column
 depositINC = dataINC |>
   select(Year, Deposit_Lost) 
 
+### Converting Table to dataframe
 depositINC = data.frame(rbind(table(depositINC)))
 
 depositINC$Year = row.names(depositINC)
 row.names(depositINC) = 1:nrow(depositINC)
 
-
+### Extracting election years from Wikipeida
 url = 'https://en.wikipedia.org/wiki/List_of_Indian_general_elections'
 
 wikidata = read_html(url) |>
@@ -35,11 +41,14 @@ wikidata = read_html(url) |>
 
 electionYear = wikidata$Election.year
 
+## Selecting Election Year 
 depositINC = depositINC[depositINC$Year %in%(electionYear),]
 row.names(depositINC) = 1:nrow(depositINC)
 
+### Getting Total no of seats contested
 depositINC$TotalSeats = depositINC$yes + depositINC$no
 
+### plotting the graph
 INCplot = ggplot(depositINC, aes(Year))+
   geom_col(aes(y =TotalSeats , fill = 'No. of Seats Contested'))+
   geom_text(aes(y = TotalSeats, label = TotalSeats),vjust = -0.25)+
@@ -64,19 +73,21 @@ INCplot = ggplot(depositINC, aes(Year))+
 
 ## BJP 
 
+### Extracting BJP data
 dataBJP = data |>
   subset(Party_ID == '1605')
 
 
 head(dataBJP)
 
+### Freq Distribution
 table(dataBJP$Deposit_Lost)
 
-
-
+### Selecting the necessary column
 depositBJP = dataBJP |>
   select(Year, Deposit_Lost) 
 
+### Converting Table to dataframe
 depositBJP = data.frame(rbind(table(depositBJP)))
 
 depositBJP$Year = row.names(depositBJP)
@@ -84,11 +95,14 @@ row.names(depositBJP) = 1:nrow(depositBJP)
 
 electionYear = wikidata$Election.year
 
+## Selecting Election Year 
 depositBJP = depositBJP[depositBJP$Year %in%(electionYear),]
 row.names(depositBJP) = 1:nrow(depositBJP)
 
+### Getting Total no of seats contested
 depositBJP$TotalSeats = depositBJP$yes + depositBJP$no
 
+### plotting the graph
 BJPplot = ggplot(depositBJP, aes(Year))+
   geom_col(aes(y =TotalSeats , fill = 'No. of Seats Contested'))+
   geom_text(aes(y = TotalSeats, label = TotalSeats),vjust = -0.25)+
